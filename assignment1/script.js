31403aa37dca8cd168d63c77371d243882f2033f
lriveracastro407 <lriveracastro407@berkeley.edu>
2021-06-11 15:52:51 -0700

    completed assignment 1

--- assignment1/script.js
+++ assignment1/script.js
@@ -1,56 +1,36 @@
 //Global Constants
-//connect html to js
-//get API information
-//create html in js
 /* HTML to JS connectors */
 const movieForm = document.querySelector('form')
 const movieArea = document.querySelector('#movie-area')
-const loadButton = document.querySelector('.hidden')
+const loadButton = document.querySelector('#loader')
 const searchinput = document.querySelector('#searchinput')
 const searchForm = document.querySelector('#searchform')
+const clearbutton = document.querySelector('#clear')
+
 /** API variables */
 const api_key = '765ece2c111fb5c30abfeb28d365ac2c'
 var pageNum = 1
 var whichload
-//tmdb images: https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US |
-//| https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
-// http://image.tmdb.org/t/p/ + configuration.images.poster_sizes[5]
-//search https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
-
-//popular -> query to show page num -> counter goes
-//img https://www.themoviedb.org/t/p/w600_and_h900_bestv2/%7Bposterpath%7D.jpg
-//get movies function
-//display movies function + innerHTML
-//handle form submit (for search)
-//load more button
 
 //Load Popular Movies
 async function popMovies() {
     whichload = 0 //loadbutton uses popMovies
     const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + api_key + '&language=en-US&page=' + pageNum)
-    //console.log(response)
     const responseData =  await response.json()
-    //console.log(responseData.results)
     getMovieinfo(responseData)
 }
-
+//Search movies
 async function searchMovies(input) {
     const searchRes = await fetch('https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&language=en-US&query=' + input +'&page=' + pageNum + '&include_adult=false')
-    console.log(searchRes)          //https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=fightclub&page=1&include_adult=false
     const searchData = await searchRes.json()
-    console.log(searchRes)
-    //getMovieinfo(searchData)
     return searchData
 }
 //Grabs movie information from API
 function getMovieinfo(data) {
     data.results.forEach(movie => {
         let title = movie.title
-        console.log(title)
         let rating = movie.vote_average
-        console.log(rating)
         let poster = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + movie.poster_path
-        console.log(poster)
         addMovies(title, rating, poster)
     });
 }
@@ -74,12 +54,12 @@ async function loadMore(event) {
         const result = await searchMovies(currSearch)
         getMovieinfo(result)
     }
+    
 }
 async function handleFormSubmit(event) {
     event.preventDefault()
     movieArea.innerHTML = ''
     currSearch = searchinput.value
-    console.log(currSearch)
     pageNum = 1
     whichload = 1 //loadbutton loads search
     const result = await searchMovies(currSearch)
@@ -89,6 +69,15 @@ async function handleFormSubmit(event) {
     
 }
 
+async function homescreen() {
+    movieArea.innerHTML = ''
+    pageNum = 1
+    popMovies() 
+
+}
+
+clearbutton.addEventListener('click', homescreen)
 loadButton.addEventListener('click', loadMore)
 searchForm.addEventListener('submit', handleFormSubmit)
 popMovies()
+
